2c7bf988282fb2109d755f96e9d6c04d9fe6820d
oi9340 <oi9340@naver.com>
2024-05-16 20:54:11 +0900

    Date, Time example

---
+++ src/time/test/TestBetween.java
@@ -0,0 +1,20 @@
+package time.test;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+public class TestBetween {
+    public static void main(String[] args) {
+        LocalDate startDate = LocalDate.of(2024, 1, 1);
+        LocalDate endDate = LocalDate.of(2024, 11, 21);
+
+        Period between = Period.between(startDate, endDate);
+        long daysBetween = ChronoUnit.DAYS.between(startDate, endDate);
+
+        System.out.println("시작 날짜 : " + startDate);
+        System.out.println("목표 날짜 : " + endDate);
+        System.out.println("남은 날짜 : " + between.getYears() + "년 " + between.getMonths() + "월 " + between.getDays() + "일");
+        System.out.println("D-DAY : " + daysBetween + "일 남음");
+    }
+}
